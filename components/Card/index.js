6e84d793cf3d8d20aef7408c7fb94cbcf58e938c
NjilaBoris <njilaboris2@gmail.com>
2026-01-04 00:51:14 +0100

    Implement Card UI

---
+++ components/Card/index.js
@@ -0,0 +1,23 @@
+export const cards = [
+  {
+    title: "Working Tours",
+    copy: " Walk in the footsteps of Entity",
+    time: "2.5HOURS",
+    price: "FROM$75",
+    backgroundVideo: "/images/1.mp4",
+  },
+  {
+    title: "Workshop",
+    copy: "Croissant-Making Workshop",
+    time: "2.5HOURS",
+    price: "FROM$75",
+    backgroundVideo: "/images/2.mp4",
+  },
+  {
+    title: "Workshop",
+    copy: "Macaron-Making Workshop",
+    time: "2.5HOURS",
+    price: "FROM$75",
+    backgroundVideo: "/images/3.mp4",
+  },
+];
